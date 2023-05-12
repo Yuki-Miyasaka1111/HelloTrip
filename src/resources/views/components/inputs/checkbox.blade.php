48f9ac426b1d5cc65b410c8e65537354110c4f4f
Yuki Miyasaka <diyhb1369@gmail.com>
2023-05-12 14:22:23 +0900

    fix: :lipstick: クラス名のコピー

--- src/resources/views/components/inputs/checkbox.blade.php
+++ src/resources/views/components/inputs/checkbox.blade.php
@@ -2,6 +2,6 @@
 
 <div class="{{ $class }}-wrap py-1 pl-1 pr-3 d-flex justify-between" style="width: {{ $width }};" >
     <span class="">{{ $label }}</span>
-    <input id="{{ $id }}" type="checkbox" name="{{ $name }}" value="{{ $value }}" class="d-none {{ $class }}" {{ $checked ? 'checked' : '' }} {!! $attributes !!}>
+    <input id="{{ $id }}" type="checkbox" name="{{ $name }}" value="{{ $value }}" class="d-none form-checkbox {{ $class }}" {{ $checked ? 'checked' : '' }} {!! $attributes !!}>
     <label for="{{ $id }}" class="{{ $class }}-label"></label>
 </div>
--- src/resources/views/components/inputs/text.blade.php
+++ src/resources/views/components/inputs/text.blade.php
@@ -1,3 +1,3 @@
 @props(['name', 'type' => 'text', 'value' => '', 'placeholder' => '', 'class' => 'form-input form-text', 'width' => '100%'])
 
-<input type="{{ $type }}" name="{{ $name }}" value="{{ $value }}" class="{{ $class }}" placeholder="{{ $placeholder }}" style="width: {{ $width }};" {!! $attributes !!}>
+<input type="{{ $type }}" name="{{ $name }}" value="{{ $value }}" class="form-input form-text {{ $class }}" placeholder="{{ $placeholder }}" style="width: {{ $width }};" {!! $attributes !!}>
