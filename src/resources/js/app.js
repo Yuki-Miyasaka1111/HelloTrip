47ea57cec1aa40cf44f687587c6dd7258d0d1b94
Yuki Miyasaka <diyhb1369@gmail.com>
2023-05-17 19:48:54 +0900

    feat: :arrow_down: WYSIWYGエディタであるQuillをインストールし、表示させた。

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -3,5 +3,6 @@ import "../css/app.scss";
 import "./components/dropdown";
 import "./components/image-upload";
 import "./components/clone";
+import "./components/quill";
 
 import "./layouts/fouc";
--- src/resources/views/partials/scripts.blade.php
+++ src/resources/views/partials/scripts.blade.php
@@ -1,2 +1,3 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
