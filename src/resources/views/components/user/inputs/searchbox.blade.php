98441d3edcc72c4d8da28eebf03a330cc52afa3a
Yuki Miyasaka <diyhb1369@gmail.com>
2023-06-15 15:44:10 +0900

    fix: :bug: 検索窓の軽微なCSSの修正

--- src/resources/views/components/user/inputs/searchbox.blade.php
+++ src/resources/views/components/user/inputs/searchbox.blade.php
@@ -1,5 +1,5 @@
 @props(['name', 'type' => 'text', 'value' => '', 'placeholder' => '', 'class' => 'form-searchbox form-text', 'width' => '100%'])
 
-<div class="d-flex items-center {{ $name }}">
+<div class="d-flex form-{{ $name }}-wrap">
     <input id="{{ $name }}" type="{{ $type }}" name="{{ $name }}" value="{{ $value }}" class="{{ $class }}" placeholder="{{ $placeholder }}" style="width: {{ $width }};" {!! $attributes !!}>
 </div>
